a0972988501f29d9fb33d1b827b84f57367b042a
Tsiry Marcel <14010248+mntm@users.noreply.github.com>
2026-01-26 16:55:53 +0300

    chore: don't copy db/oravox.sqlite anymore

--- docker/copier/init.sh
+++ docker/copier/init.sh
@@ -7,11 +7,6 @@ set -o pipefail
 
 DEST_DIR=${WWW_DIR:-"/var/www/html"}
 
-if [ -z "$(ls -A $DEST_DIR/db/)" ]; then
-    cp /db/oravox.sqlite $DEST_DIR/db/
-    chmod -R ugo+w $DEST_DIR/db
-fi
-
 if [ -z "$(ls -A $DEST_DIR/assets/stories/)" ]; then
     cp -rt $DEST_DIR/assets/ /stories
     chmod -R ugo+w $DEST_DIR/assets/stories/
